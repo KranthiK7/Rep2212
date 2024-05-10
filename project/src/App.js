6a5a1e2365aa3f9f5e37f86d4d9367feb0c80a96
Kranthi <114945767+KranthiK7@users.noreply.github.com>
2024-05-10 23:14:08 +0530

    added another heading

--- project/src/App.js
+++ project/src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
         <h1> This heading added by the kranthi from local repository </h1>
+        <h1> This is heading added by other developer </h1>
     </div>
   );
 }
