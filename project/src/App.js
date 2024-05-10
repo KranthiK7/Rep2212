164ba1d9610de12a697f81c2eef16a9212e71475
kranthik <kranthik694@gmail.com>
2024-05-10 23:01:47 +0530

    added heading

--- project/src/App.js
+++ project/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>Hello React </h1>
+        <h1> This heading added by the kranthi from local repository </h1>
     </div>
   );
 }
